2b7a508429c581e5166c741549efb3455dcd15a2
Iver Dihle Skjervum <iver.ds@gmail.com>
2012-03-11 13:14:10 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StageMe::Application.config.secret_token = '09e46af1e6c1e0dcb9d7b0af5393fde362a42b9916aaf5dae7b5db69de1284aae0cddec57c1d623da3f8bfa159956de8aab19fe2b22c4682bd734dadb8b86f55'
